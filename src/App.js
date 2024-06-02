1444c24b1e3efcc208f8cc2d93b1e5d8f58c0bbd
lexifrost <alexa.frost@outlook.com>
2024-06-02 15:50:05 +0200

    Added the GitHub repo url

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by Lexi Frost and is{" "}
+        <a
+          href="https://github.com/lexifrost/react-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
